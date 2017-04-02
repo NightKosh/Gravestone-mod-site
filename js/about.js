99e84d4bde474a15f9c407043b0971310406c47c
NightKosh <nightkosh@gmail.com>
2017-04-02 22:41:17 +0300

    Memorials info, #3

--- js/about.js
+++ js/about.js
@@ -28,14 +28,87 @@ app.controller("AboutCtrl", ['$scope', '$http', 'Lightbox', 'PageService', funct
                 ];
             $scope.about.gravesEffectsImg = [
                     {'thumbUrl': 'images/img/graves/flowers.png', 'url': 'images/img/graves/flowers.png', 'caption': ''},
-                    {'thumbUrl': 'images/img/graves/mossy_graves.jpg', 'url': 'images/img/graves/mossy_graves.jpg', 'caption': ''},
-                    {'thumbUrl': 'images/img/graves/mossy_graves.png', 'url': 'images/img/graves/mossy_graves.png', 'caption': ''}
+                    {'thumbUrl': 'images/img/graves/mossy_graves.jpg', 'url': 'images/img/graves/mossy_graves.jpg', 'caption': ''}
                 ];
         });
 
         $scope.openLightboxModal = function (images, index) {
             Lightbox.openModal(images, index);
         };
+    }
+}]);
+
+app.controller("AboutMemorialCtrl", ['$scope', '$http', 'Lightbox', 'PageService', function($scope, $http, Lightbox, PageService) {
+    $scope.about = {};
+
+    if (PageService.getPageParams().page == "about") {
+        $http.get(PageService.getDataPath()).success(function (data, status, headers, config) {
+            $scope.about = data;
+
+            $scope.about.memorialsImg = [
+                {'thumbUrl': 'images/img/memorials/cross_1.png', 'url': 'images/img/memorials/cross_1.png', 'caption': ''},
+                {'thumbUrl': 'images/img/memorials/cross_2.png', 'url': 'images/img/memorials/cross_2.png', 'caption': ''},
+                {'thumbUrl': 'images/img/memorials/cross_3.png', 'url': 'images/img/memorials/cross_3.png', 'caption': ''},
+                {'thumbUrl': 'images/img/memorials/cross_4.png', 'url': 'images/img/memorials/cross_4.png', 'caption': ''},
+                {'thumbUrl': 'images/img/memorials/obelisk_1.png', 'url': 'images/img/memorials/obelisk_1.png', 'caption': ''},
+                {'thumbUrl': 'images/img/memorials/obelisk_2.png', 'url': 'images/img/memorials/obelisk_2.png', 'caption': ''},
+                {'thumbUrl': 'images/img/memorials/obelisk_3.png', 'url': 'images/img/memorials/obelisk_3.png', 'caption': ''},
+                {'thumbUrl': 'images/img/memorials/obelisk_4.png', 'url': 'images/img/memorials/obelisk_4.png', 'caption': ''},
+                {'thumbUrl': 'images/img/memorials/celtic_1.png', 'url': 'images/img/memorials/celtic_1.png', 'caption': ''},
+                {'thumbUrl': 'images/img/memorials/celtic_2.png', 'url': 'images/img/memorials/celtic_2.png', 'caption': ''},
+                {'thumbUrl': 'images/img/memorials/celtic_3.png', 'url': 'images/img/memorials/celtic_3.png', 'caption': ''},
+                {'thumbUrl': 'images/img/memorials/steve_1.png', 'url': 'images/img/memorials/steve_1.png', 'caption': ''},
+                {'thumbUrl': 'images/img/memorials/steve_2.png', 'url': 'images/img/memorials/steve_2.png', 'caption': ''},
+                {'thumbUrl': 'images/img/memorials/steve_3.png', 'url': 'images/img/memorials/steve_3.png', 'caption': ''},
+                {'thumbUrl': 'images/img/memorials/villagers_1.png', 'url': 'images/img/memorials/villagers_1.png', 'caption': ''},
+                {'thumbUrl': 'images/img/memorials/villagers_2.png', 'url': 'images/img/memorials/villagers_2.png', 'caption': ''},
+                {'thumbUrl': 'images/img/memorials/villagers_3.png', 'url': 'images/img/memorials/villagers_3.png', 'caption': ''},
+                {'thumbUrl': 'images/img/memorials/angel_1.png', 'url': 'images/img/memorials/angel_1.png', 'caption': ''},
+                {'thumbUrl': 'images/img/memorials/angel_2.png', 'url': 'images/img/memorials/angel_2.png', 'caption': ''},
+                {'thumbUrl': 'images/img/memorials/angel_3.png', 'url': 'images/img/memorials/angel_3.png', 'caption': ''},
+                {'thumbUrl': 'images/img/memorials/dog_1.png', 'url': 'images/img/memorials/dog_1.png', 'caption': ''},
+                {'thumbUrl': 'images/img/memorials/dog_2.png', 'url': 'images/img/memorials/dog_2.png', 'caption': ''},
+                {'thumbUrl': 'images/img/memorials/dog_3.png', 'url': 'images/img/memorials/dog_3.png', 'caption': ''},
+                {'thumbUrl': 'images/img/memorials/cat_1.png', 'url': 'images/img/memorials/cat_1.png', 'caption': ''},
+                {'thumbUrl': 'images/img/memorials/cat_2.png', 'url': 'images/img/memorials/cat_2.png', 'caption': ''},
+                {'thumbUrl': 'images/img/memorials/cat_3.png', 'url': 'images/img/memorials/cat_3.png', 'caption': ''},
+                {'thumbUrl': 'images/img/memorials/creeper_1.png', 'url': 'images/img/memorials/creeper_1.png', 'caption': ''},
+                {'thumbUrl': 'images/img/memorials/creeper_2.png', 'url': 'images/img/memorials/creeper_2.png', 'caption': ''},
+                {'thumbUrl': 'images/img/memorials/creeper_3.png', 'url': 'images/img/memorials/creeper_3.png', 'caption': ''}
+            ];
+            $scope.about.memorialsMossyImg = [
+                {'thumbUrl': 'images/img/memorials/mossy_1.png', 'url': 'images/img/memorials/mossy_1.png', 'caption': ''},
+                {'thumbUrl': 'images/img/memorials/mossy_2.png', 'url': 'images/img/memorials/mossy_2.png', 'caption': ''},
+                {'thumbUrl': 'images/img/memorials/mossy_3.png', 'url': 'images/img/memorials/mossy_3.png', 'caption': ''}
+            ];
+        });
+
+        $scope.openLightboxModal = function (images, index) {
+            Lightbox.openModal(images, index);
+        };
+    }
+}]);
+
+app.controller("AboutMenuCtrl", ['$scope', '$http', 'PageService', function($scope, $http, PageService) {
+    $scope.aboutMenu = {};
+
+    if (PageService.getPageParams().page == "about") {
+        $http.get(PageService.getSubMenuDataPath()).success(function (data, status, headers, config) {
+            $scope.aboutMenu = data;
+
+
+            $scope.aboutMenu.link_gravestone = PageService.getLinkWithLangParams("?page=about");
+            $scope.aboutMenu.link_memorial = PageService.getLinkWithLangParams("?page=about&sub_page=memorial");
+            $scope.aboutMenu.link_execution = PageService.getLinkWithLangParams("?page=about&sub_page=execution");
+            $scope.aboutMenu.link_candle = PageService.getLinkWithLangParams("?page=about&sub_page=candle");
+            $scope.aboutMenu.link_spawner = PageService.getLinkWithLangParams("?page=about&sub_page=spawner");
+            $scope.aboutMenu.link_blocks = PageService.getLinkWithLangParams("?page=about&sub_page=blocks");
+            $scope.aboutMenu.link_copses = PageService.getLinkWithLangParams("?page=about&sub_page=copses");
+            $scope.aboutMenu.link_mobs = PageService.getLinkWithLangParams("?page=about&sub_page=mobs");
+            $scope.aboutMenu.link_structures = PageService.getLinkWithLangParams("?page=about&sub_page=structures");
+            $scope.aboutMenu.link_commands = PageService.getLinkWithLangParams("?page=about&sub_page=commands");
+            $scope.aboutMenu.link_configs = PageService.getLinkWithLangParams("?page=about&sub_page=configs");
+        });
     }
 }]);
 
