f3cc657ec3d89aee372689d43fefa7a367a42024
NightKosh <nightkosh@gmail.com>
2017-04-03 04:28:17 +0300

    structures info, #3

--- js/about.js
+++ js/about.js
@@ -3,7 +3,7 @@
 app.controller("AboutCtrl", ['$scope', '$http', 'Lightbox', 'PageService', function($scope, $http, Lightbox, PageService) {
     $scope.about = {};
 
-    if (PageService.getPageParams().page == "about") {
+    if (PageService.getPageParams().page == "about" && PageService.getPageParams().sub_page == null) {
         $http.get(PageService.getDataPath()).success(function (data, status, headers, config) {
             $scope.about = data;
 
@@ -41,7 +41,7 @@ app.controller("AboutCtrl", ['$scope', '$http', 'Lightbox', 'PageService', funct
 app.controller("AboutMemorialCtrl", ['$scope', '$http', 'Lightbox', 'PageService', function($scope, $http, Lightbox, PageService) {
     $scope.about = {};
 
-    if (PageService.getPageParams().page == "about") {
+    if (PageService.getPageParams().page == "about" && PageService.getPageParams().sub_page == "memorial") {
         $http.get(PageService.getDataPath()).success(function (data, status, headers, config) {
             $scope.about = data;
 
@@ -80,6 +80,66 @@ app.controller("AboutMemorialCtrl", ['$scope', '$http', 'Lightbox', 'PageService
                 {'thumbUrl': 'images/img/memorials/mossy_1.png', 'url': 'images/img/memorials/mossy_1.png', 'caption': ''},
                 {'thumbUrl': 'images/img/memorials/mossy_2.png', 'url': 'images/img/memorials/mossy_2.png', 'caption': ''},
                 {'thumbUrl': 'images/img/memorials/mossy_3.png', 'url': 'images/img/memorials/mossy_3.png', 'caption': ''}
+            ];
+        });
+
+        $scope.openLightboxModal = function (images, index) {
+            Lightbox.openModal(images, index);
+        };
+    }
+}]);
+
+app.controller("AboutStructuresCtrl", ['$scope', '$http', 'Lightbox', 'PageService', function($scope, $http, Lightbox, PageService) {
+    $scope.about = {};
+
+    if (PageService.getPageParams().page == "about" && PageService.getPageParams().sub_page == "structures") {
+        $http.get(PageService.getDataPath()).success(function (data, status, headers, config) {
+            $scope.about = data;
+
+            $scope.about.structuresImg = [
+                {'thumbUrl': 'images/img/structures/thumb/grave.png', 'url': 'images/img/structures/grave.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/thumb/opened_grave.png', 'url': 'images/img/structures/opened_grave.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/thumb/memorial.png', 'url': 'images/img/structures/memorial.png', 'caption': ''}
+            ];
+            $scope.about.villageImg = [
+                {'thumbUrl': 'images/img/structures/village/thumb/cemetery_1.png', 'url': 'images/img/structures/village/cemetery_1.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/village/thumb/cemetery_2.png', 'url': 'images/img/structures/village/cemetery_2.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/village/thumb/undertaker_1.png', 'url': 'images/img/structures/village/undertaker_1.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/village/thumb/undertaker_2.png', 'url': 'images/img/structures/village/undertaker_2.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/village/thumb/undertaker_3.png', 'url': 'images/img/structures/village/undertaker_3.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/village/thumb/undertaker_4.png', 'url': 'images/img/structures/village/undertaker_4.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/village/thumb/undertaker_5.png', 'url': 'images/img/structures/village/undertaker_5.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/village/thumb/undertaker_6.png', 'url': 'images/img/structures/village/undertaker_6.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/village/thumb/undertaker_7.png', 'url': 'images/img/structures/village/undertaker_6.png', 'caption': ''}
+            ];
+            $scope.about.catacombsImg = [
+                {'thumbUrl': 'images/img/structures/catacombs/thumb/1.png', 'url': 'images/img/structures/catacombs/1.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/catacombs/thumb/2.png', 'url': 'images/img/structures/catacombs/2.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/catacombs/thumb/3.png', 'url': 'images/img/structures/catacombs/3.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/catacombs/thumb/4.png', 'url': 'images/img/structures/catacombs/4.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/catacombs/thumb/5.png', 'url': 'images/img/structures/catacombs/5.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/catacombs/thumb/6.png', 'url': 'images/img/structures/catacombs/6.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/catacombs/thumb/7.png', 'url': 'images/img/structures/catacombs/7.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/catacombs/thumb/8.png', 'url': 'images/img/structures/catacombs/8.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/catacombs/thumb/9.png', 'url': 'images/img/structures/catacombs/9.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/catacombs/thumb/10.png', 'url': 'images/img/structures/catacombs/10.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/catacombs/thumb/11.png', 'url': 'images/img/structures/catacombs/11.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/catacombs/thumb/12.png', 'url': 'images/img/structures/catacombs/12.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/catacombs/thumb/13.png', 'url': 'images/img/structures/catacombs/13.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/catacombs/thumb/14.png', 'url': 'images/img/structures/catacombs/14.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/catacombs/thumb/15.png', 'url': 'images/img/structures/catacombs/15.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/catacombs/thumb/16.png', 'url': 'images/img/structures/catacombs/16.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/catacombs/thumb/17.png', 'url': 'images/img/structures/catacombs/17.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/catacombs/thumb/18.png', 'url': 'images/img/structures/catacombs/18.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/catacombs/thumb/19.png', 'url': 'images/img/structures/catacombs/19.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/catacombs/thumb/20.png', 'url': 'images/img/structures/catacombs/20.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/catacombs/thumb/21.png', 'url': 'images/img/structures/catacombs/21.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/catacombs/thumb/22.png', 'url': 'images/img/structures/catacombs/22.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/catacombs/thumb/23.png', 'url': 'images/img/structures/catacombs/23.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/catacombs/thumb/24.png', 'url': 'images/img/structures/catacombs/24.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/catacombs/thumb/25.png', 'url': 'images/img/structures/catacombs/25.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/catacombs/thumb/26.png', 'url': 'images/img/structures/catacombs/26.png', 'caption': ''},
+                {'thumbUrl': 'images/img/structures/catacombs/thumb/27.png', 'url': 'images/img/structures/catacombs/27.png', 'caption': ''}
             ];
         });
 
